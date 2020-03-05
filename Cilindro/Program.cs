cd31e09235ea65d7fa63a2fb02678b17ac1e1676
João Borges <joaonunos.borges@gmail.com>
2020-03-05 16:07:33 +0000

    Ex5

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -8,7 +8,7 @@ namespace Cilindro
         {
             string sa, sr;
             float aF,rF;
-            float pi = 3.1415926f;
+
             Console.WriteLine("Pf escreva a altura e raio do cilindro");
 
             sa = Console.ReadLine();
@@ -16,8 +16,8 @@ namespace Cilindro
             aF = float.Parse(sa);
             rF = float.Parse(sr);
 
-            float VF = pi * (rF * 2 * 2) * aF;
-            float ASF = 2 * pi * rF * (rF + aF);
+            float VF = (float)Math.PI * (float)Math.Pow(rF,2) * aF;
+            float ASF = 2 * (float)Math.PI * rF * (rF + aF);
 
             Console.WriteLine($"Volume = {VF}");
             Console.WriteLine($"Area de superficie = {ASF}");
