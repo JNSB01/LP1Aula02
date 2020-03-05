75e5c2b9ee1f8e370a7d81d750ad7e90bfae6e09
João Borges <joaonunos.borges@gmail.com>
2020-03-05 16:42:54 +0000

    Ex6

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dM = double.MaxValue;
+            float fM = float.MaxValue;
+            int iM = int.MaxValue;
+            long lM = long.MaxValue;
+            short sM = short.MaxValue;
+            byte bM = byte.MaxValue;
+
+            Console.WriteLine(dM);
+            Console.WriteLine(fM);
+            Console.WriteLine(iM);
+            Console.WriteLine(lM);
+            Console.WriteLine(sM);
+            Console.WriteLine(bM);
+        }
+    }
+}
