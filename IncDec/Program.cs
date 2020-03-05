e48968f0326d7253a07b86990ec3b6e760ea1c4a
João Borges <joaonunos.borges@gmail.com>
2020-03-05 14:43:36 +0000

    Ex1

--- IncDec/Program.cs
+++ IncDec/Program.cs
@@ -7,8 +7,12 @@ namespace IncDec
         static void Main(string[] args)
         {
             int x = 5;
-            x++;
+            int y = x++;
             Console.WriteLine(x);
+            Console.WriteLine(y);
+            int z = --y;
+            Console.WriteLine(z);
+            Console.WriteLine(y);
         }
     }
 }
